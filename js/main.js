eb2074dbf9e3a11e8390880cf052d56ba0721ba0
Nicolò Pincelli <pincelli.n@gmail.com>
2024-06-06 15:31:57 +0200

    Add div container and class by js

--- js/main.js
+++ js/main.js
@@ -4,16 +4,28 @@
 for (let i = 1; i <= 100; i++) {
   let element = i;
   //console.log(element);
-  
+ //creo un contenitore div
+const Box = document.createElement("div");
+//aggiungo la classe con le caratteristiche di base che voglio
+ Box.classList.add("Square");
   // creo un if in cuoi metto a confronto gli elementi e richiamo le operazioni richieste
-
+//eseguo come prima operazione il doppio confronto
   if (element % 3 == 0 && element % 5 == 0) {
+    // il risultato prende il valore indicato
     element ="FizzBuzz";
+    //il risultato ottiene questa classe
+    Box.classList.add("fizzbuzz");
+    
   } else if (element % 3 == 0) {
     element ="Fizz";
+    Box.classList.add("fizz");
   } else if (element % 5 == 0) {
     element ="Buzz";
+    Box.classList.add("buzz");
   } 
-
+  
+  const TextBox = element;
+  Box.append(TextBox);
+  document.getElementById("container").appendChild(Box);
   console.log(element);
 }
